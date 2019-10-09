52af1195d99b2e0663c2faf7c4ca919e5005b855
Chutong Lin <30555643+RobinLCT@users.noreply.github.com>
2019-10-08 22:56:45 -0500

    Update SurfaceType.java

--- CleanSweep_Group6/src/main/java/edu/depaul/cleanSweep/cell/SurfaceType.java
+++ CleanSweep_Group6/src/main/java/edu/depaul/cleanSweep/cell/SurfaceType.java
@@ -2,7 +2,7 @@ package edu.depaul.cleanSweep.cell;
 
 public enum SurfaceType {
 	BARE(1), LOWPILE(2), HIGHPILE(3);
-	
+	//abbr indicates how many units of charge this surface will consume.
 	private int abbr;
 	
 	SurfaceType(int abb) {
