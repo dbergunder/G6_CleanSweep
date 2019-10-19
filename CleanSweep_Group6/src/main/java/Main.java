9d19ebf6c2e3c12dede47db174e0f8c5a0d63b5f
dnicolemy <dana.myers15@gmail.com>
2019-10-18 21:49:03 -0500

    main method added

---
+++ CleanSweep_Group6/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import edu.depaul.cleanSweep.controlSystem.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("here");
+		Cleaner myCleaner = new Cleaner();
+		//Create Cleaner
+		//Create floor 
+	}
+
+}
